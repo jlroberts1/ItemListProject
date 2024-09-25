856d86be5acf01d62f153e9de50ace37de742cea
James Roberts <105246977+jlroberts1@users.noreply.github.com>
2024-09-25 18:44:52 -0400

    Update sorting of items and add additional tests to cover more cases

--- app/src/main/java/com/jlroberts1/itemlistproject/presentation/items/ItemsViewModel.kt
+++ app/src/main/java/com/jlroberts1/itemlistproject/presentation/items/ItemsViewModel.kt
@@ -34,11 +34,8 @@ class ItemsViewModel @Inject constructor(
             onComplete = { _state.update { it.copy(loading = false, error = null) } },
             onError = { error -> _state.update { it.copy(loading = false, error = error) } }
         ).map {
-            it.filter { item ->
-                item.listId == listId && !item.name.isNullOrBlank()
-            }.sortedBy { item ->
-                item.name?.let { name -> geItemNumbers(name) }
-            }
+            it.filter { item -> item.listId == listId && !item.name.isNullOrBlank() }
+                .sortedWith { first, second -> compareNames(first.name!!, second.name!!) }
         }
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), emptyList())
 
@@ -46,7 +43,45 @@ class ItemsViewModel @Inject constructor(
         this.listId.update { listId }
     }
 
-    private fun geItemNumbers(name: String): Int {
-        return Integer.parseInt(name.filter { chars -> chars.isDigit() })
+    private fun compareNames(first: String, second: String): Int {
+        val name1Parts = splitNamesIntoSegments(first)
+        val name2Parts = splitNamesIntoSegments(second)
+
+        for (i in name1Parts.indices) {
+            if (i > name2Parts.size) return 1
+            val compare = compareSegments(name1Parts[i], name2Parts[i])
+            if (compare != 0) return compare
+        }
+        return name1Parts.size.compareTo(name2Parts.size)
+    }
+
+
+    private fun splitNamesIntoSegments(name: String): List<String> {
+        val segments = mutableListOf<String>()
+        val curr = StringBuilder()
+        var isDigitSegment = name[0].isDigit()
+
+        for (char in name) {
+            if (char.isDigit() == isDigitSegment) {
+                curr.append(char)
+            } else {
+                segments.add(curr.toString())
+                curr.clear()
+                curr.append(char)
+                isDigitSegment = char.isDigit()
+            }
+        }
+
+        segments.add(curr.toString())
+        return segments
+    }
+
+    private fun compareSegments(seg1: String, seg2: String): Int {
+        val isSeg1Numeric = seg1.all { it.isDigit() }
+        val isSeg2Numeric = seg2.all { it.isDigit() }
+        return when(isSeg1Numeric && isSeg2Numeric) {
+            true -> seg1.toInt().compareTo(seg2.toInt())
+            false -> seg1.compareTo(seg2)
+        }
     }
 }
--- app/src/test/java/com/jlroberts1/itemlistproject/ui/theme/items/ItemsViewModelTest.kt
+++ app/src/test/java/com/jlroberts1/itemlistproject/ui/theme/items/ItemsViewModelTest.kt
@@ -45,7 +45,6 @@ class ItemsViewModelTest {
             Item(listId = 1, id = 3, name = "Item 3"),
             Item(listId = 2, id = 4, name = "Item 4"),
             Item(listId = 1, id = 5, name = "Item 5")
-
         )
 
         coEvery { itemsRepository.getItems(capture(onComplete), capture(onError)) } answers {
@@ -59,6 +58,46 @@ class ItemsViewModelTest {
             Item(1, 1, "Item 1"),
             Item(3, 1, "Item 3"),
             Item(5, 1, "Item 5")
+        )
+
+        viewModel.items.test {
+            viewModel.setListId(1)
+            val item1 = awaitItem()
+            assertThat(item1).isEqualTo(emptyList<Item>())
+            val item2 = awaitItem()
+            assertThat(item2).isEqualTo(expected)
+        }
+    }
+
+    @Test
+    fun `items should be in the correct order after sorting`() = runTest {
+        val mockItems = listOf(
+            Item(listId = 1, id = 1, name = "Hanes 1"),
+            Item(listId = 1, id = 2, name = "James 2"),
+            Item(listId = 1, id = 3, name = "Item 300"),
+            Item(listId = 1, id = 4, name = "Item 20000"),
+            Item(listId = 1, id = 5, name = "Item 20"),
+            Item(listId = 1, id = 6, name = "Items 22"),
+            Item(listId = 1, id = 7, name = "Items 301"),
+            Item(listId = 1, id = 8, name = "Item 21")
+        )
+
+        coEvery { itemsRepository.getItems(capture(onComplete), capture(onError)) } answers {
+            onComplete.captured.invoke()
+            flowOf(mockItems)
+        }
+
+        val viewModel = ItemsViewModel(itemsRepository)
+
+        val expected = listOf(
+            Item(listId = 1, id = 1, name = "Hanes 1"),
+            Item(listId = 1, id = 5, name = "Item 20"),
+            Item(listId = 1, id = 8, name = "Item 21"),
+            Item(listId = 1, id = 3, name = "Item 300"),
+            Item(listId = 1, id = 4, name = "Item 20000"),
+            Item(listId = 1, id = 6, name = "Items 22"),
+            Item(listId = 1, id = 7, name = "Items 301"),
+            Item(listId = 1, id = 2, name = "James 2"),
         )
 
         viewModel.items.test {
